fe74f2bf5c1acb521eb8172868e1b6b98830afe0
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-27 19:30:44 +0200

    req,res

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 //backend
 const app = express();
 const PORT = 5000;
@@ -8,10 +10,8 @@ const PORT = 5000;
 //middleware
 app.use(bodyParser.json()); //sending and requesting data trhough rest api
 
-app.get('/', (req, res) => {
-    console.log('[TEST]');
+app.use('/users', usersRoutes);
 
-    res.send('Hello from Homepage.');
-});
+app.get('/', (req, res) =>  res.send('Hello from Homepage.')); 
 
 app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
