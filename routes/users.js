ee7ba66db49f4f2d7f794150cd7ef35c7095c454
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-27 20:06:34 +0200

    data

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Fda",
+        age: 23
+    },
+    {
+        firstName: "Jelly",
+        lastName: "Beans",
+        age: 33
+    }
+]
 
 router.get('/', (req, res) => {
+    console.log(users);
     res.send('Hello');
 });
 
